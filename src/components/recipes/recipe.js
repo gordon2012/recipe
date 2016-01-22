ba751d6c8758a9ac7966b2dd3f10d8f9406073ec
Gordon Doskas <gdoskas2012@email.phoenix.edu>
2016-01-22 00:41:58 -0800

    implement accordion transitions

--- src/components/recipes/recipe.js
+++ src/components/recipes/recipe.js
@@ -13,18 +13,18 @@ var Recipe = React.createClass
 	render: function()
 	{
 		var className = this.props.open ? 'recipe open' : 'recipe';
-		var details;
-		if(this.props.open)
-		{
-			details = <RecipeDetails data={this.props.data}/>;
-		}
+		// var details;
+		// if(this.props.open)
+		// {
+		// 	details = <RecipeDetails data={this.props.data}/>;
+		// }
 
 		return (
 			<div className={className}>
 				<div className="recipe-heading" onClick={this.handleClick}>{this.props.data.name}</div>
 				<div className="detail-wrap">
 					<div className="detail">
-						{details}
+						<RecipeDetails data={this.props.data}/>
 					</div>
 				</div>
 			</div>
