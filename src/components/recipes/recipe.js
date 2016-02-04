905d9bb49985b7f8b5c9e3dc38e991cad37104d5
Gordon Doskas <gdoskas2012@email.phoenix.edu>
2016-02-04 09:15:02 -0800

    add buttons to recipe

--- src/components/recipes/recipe.js
+++ src/components/recipes/recipe.js
@@ -12,15 +12,34 @@ var Recipe = React.createClass
 
 	render: function()
 	{
-		var className = this.props.open ? 'recipe open' : 'recipe';
+		var classTop, classWrap, classDetail;
+
+		if(this.props.open)
+		{
+			classTop = 'recipe open';
+			classWrap = 'detail-wrap margin-8 border-r';
+			classDetail = 'detail margin-8 border-g';
+		}
+		else
+		{
+			classTop = 'recipe';
+			classWrap = 'detail-wrap';
+			classDetail = 'detail';
+		}
+
+
 
 		return (
-			<div className={className}>
+			<div className={classTop}>
 				<div className="recipe-heading" onClick={this.handleClick}>{this.props.data.name}</div>
-				<div className="detail-wrap">
-					<div className="detail">
+				<div className={classWrap}>
+					<h4>Detail Wrap</h4>
+					<div className={classDetail}>
+						Detail
 						<RecipeDetails data={this.props.data}/>
+						End detail
 					</div>
+					<button className="btn btn-danger">Delete</button><button className="btn btn-default">Edit</button>
 				</div>
 			</div>
 		);
